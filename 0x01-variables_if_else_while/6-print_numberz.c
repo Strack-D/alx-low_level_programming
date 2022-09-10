8a7ee4462c4a0b8f3b21ebb77e725d5798c37947
Strack-D <oluwatobi023@gmail.com>
2022-09-09 21:59:37 -0700

    hey hey

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,18 +1,15 @@
 #include <stdio.h>
 
 /**
- * main - print base 16 numbers
+ * main - print out numbers
  * Return: Always 0 (success)
  */
 int main(void)
 {
-	char ch;
 	int no;
 
 	for (no = 0; no <= 9; no++)
 		putchar(no);
-	for (ch = 'a'; ch <= 'f'; ch++)
-		putchar(ch);
-	putchar("\n");
+	putchat("\n");
 	return(0);
 }
