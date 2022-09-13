8d2df33de37e052f66419e95187d80cb4f96fb14
Strack-D <oluwatobi023@gmail.com>
2022-09-12 18:37:43 -0700

    hey hey

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -3,37 +3,37 @@
 
 
 /**
+
+ * main - Entry point
+
  *
- *  * main - Entry point
- *
- *   *
- *
- *    * Return: Always 0 (Success)
- *
- *     */
+
+ * Return: Always 0 (Success)
+
+ */
 
 int main(void)
 
 {
 
-	 int d = 48;
+ int d = 48;
 
 
 
-	  while (d < 58)
+ while (d < 58)
 
-		   {
+ {
 
-			     putchar(d);
+  putchar(d);
 
-			       d++;
+  d++;
 
-			        }
+ }
 
-	   putchar('\n');
+ putchar('\n');
 
 
 
-	    return (0);
+ return (0);
 
 }
