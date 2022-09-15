f654ca74a34fdde18e4fc33ba744fac25c661562
Strack-D <oluwatobi023@gmail.com>
2022-09-14 20:25:08 -0700

    fixed task 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
  * print_alphabet_x10 - print the alphabets ten times
@@ -11,10 +10,10 @@ void print_alphabet_x10(void)
 	int ten;
 	char ch;
 
-	for (ten = 0; ten < 10; ten++)
+	for (ten = 0; ten < 10; ++ten)
 	{
 		for (ch = 'a'; ch <= 'z'; ch++)
-			_putchar(ch);
+			putchar(ch);
 	}
-	_putchar('\n');
+	putchar('\n');
 }
